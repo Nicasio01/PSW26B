d9081aac655daccd99b3b562e24ff49f73b1ffcc
Nicasio01 <angel.nicasio@alumnos.udg.mx>
2026-09-06 16:25:48 -0600

    Pequeña avance

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,11 @@ Route::get('/', function () {
 Route::view('/admin', '/administradores');
 Route::view('/clnt', '/clientes');
 Route::view('/home', '/home');
+Route::view('/layout', '/layout/app');
+Route::view('/catalogo', '/catalogo/empleos');
 
+Route::get('/', function () { return view('layout'); });
+Route::get('/categorias', function () { return view('categorias.index'); })->name('categorias.index');
+Route::get('/categorias/crear', function () { return view('categorias.create'); })->name('categorias.create');
+Route::get('/ofertas', function () { return view('ofertas.index'); })->name('ofertas.index');
+Route::get('/ofertas/crear', function () { return view('ofertas.create'); })->name('ofertas.create');
